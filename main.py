6e0295b18ef3460674b47b42045b65f53c7e9e8b
jiutian303jeff <youxizhu80@gmail.com>
2025-12-27 11:58:25 -0500

    a

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 
 import pygame
+import sprite
 
 class Main:
     def __init__(self):
         pygame.init()
         # D - Display configuration
-        self.screen = pygame.display.set_mode((1920, 480))
+        self.screen = pygame.display.set_mode((1920, 1080))
         pygame.display.set_caption("Hello, world!")
         # E - Entities
         self.entities()
@@ -17,13 +18,14 @@ class Main:
         self.loop()
         # Close the game window , set the X
         pygame.quit()
+
     def entities(self):
-    """Set background for your game"""
+        """Set background for game"""
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
         self.background.fill((0, 0, 255))
-        def loop(self):
-    """ Run infinite loop until user quit"""
+
+    def loop(self):
         keepGoing = True
         while keepGoing:
             # T - Timer to set the frame rate
---
+++ sprite.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Player:
+    pass
+
+class Enemy:
+    pass
+
+class Blade:
+    pass
+
+class Bullet:
+    pass
+
+class Background:
+    pass
+
+class Weapons:
+    pass
+
+class Physics:
+    pass
+
+class Obstacle:
+    pass
