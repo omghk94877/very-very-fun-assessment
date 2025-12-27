2484ef2c825fadd42330905b91265effdc67c2fa
jiutian303jeff <youxizhu80@gmail.com>
2025-12-27 13:32:45 -0500

    new

--- sprite.py
+++ sprite.py
@@ -1,25 +1,73 @@
 import pygame
 
-class Player:
+class Player(pygame.sprite.Sprite):
+    """
+    Player is not really moving, instead it's the backgound moving. 
+    player only moves when the background reach the edge
+    """
+
+    def __init__(self, screen):
+        pygame.sprite.Sprite.__init__(self)
+
+        #images of different actions
+        self.stand = "image"
+        self.move_l = "image"
+        self.move_r = "image"
+        self.jump = "image"
+        self.attack = "image"
+        self.die = "image"
+
+
+        #starting image is stand
+        self.image = self.stand
+
+        #setting rect for collision
+        self.rect = self.image.get_rect()
+
+        #displacement
+        self.dx = 0
+        self.dy = 0
+
+    def move_left(self):
+        self.image = self.move_l
+
+    def move_right(self):
+        self.image = self.move_r
+
+    def move_up(self):
+        self.image = self.jump
+        
+
+    def death(self):
+        self.image = self.die
+        
+
+    def att(self):
+        self.image = self.attack
+        
+
+    def update(self):
+        pass
+
+
+
+class Enemy(pygame.sprite.Sprite):
     pass
 
-class Enemy:
+class Blade(pygame.sprite.Sprite):
     pass
 
-class Blade:
+class Bullet(pygame.sprite.Sprite):
     pass
 
-class Bullet:
+class Background(pygame.sprite.Sprite):
     pass
 
-class Background:
+class Weapons(pygame.sprite.Sprite):
     pass
 
-class Weapons:
+class Physics(pygame.sprite.Sprite):
     pass
 
-class Physics:
-    pass
-
-class Obstacle:
+class Obstacle(pygame.sprite.Sprite):
     pass
